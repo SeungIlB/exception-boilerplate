11ba93b3f5ec8e113bd8f2937ae961728cb673cf
승일 <seoung1515@naver.com>
2025-07-30 11:24:41 +0900

    Initial: 최초커밋

---
+++ src/main/java/seungil/exception_boilerplate/ExceptionBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package seungil.exception_boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seungil/exception_boilerplate/ExceptionBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package seungil.exception_boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
